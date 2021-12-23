e5155117e26fc9a2de8cdbd44924c4977e03cea1
Sukalyan <sukalyandash1920@gmail.com>
2021-12-23 19:41:53 +0530

    added UC-2.sh

---
+++ UC-2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+isPresent=1
+randomCheck=$((RANDOM%2))
+checkPresent=1
+if [$ispresent -eq $randomcheck ]
+then
+        empRatePerHr=20
+        empHrs=8
+        salary=$(($empHrs*$empRatePerHr))
+else
+        salary=0
+fi
